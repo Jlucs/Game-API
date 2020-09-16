c7ae18678f900abef065dd929901139fa316f635
jlucs <janislucs@inbox.lv>
2020-09-16 09:31:04 +0300

    Initial release

---
+++ database/migrations/2020_09_15_111141_create_game_reviews_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGameReviewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('game_reviews', function (Blueprint $table) {
+            $table->id();
+            $table->integer('game_id');
+            $table->integer('review_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('game_reviews');
+    }
+}
---
+++ routes/api/v1/api.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+// Users
+
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\GameController;
+use App\Http\Controllers\ReviewController;
+use App\Http\Controllers\GameReviewController;
+
+Route::resource('games', GameController::class);
+Route::resource('reviews', ReviewController::class);
+Route::resource('games.reviews', GameReviewController::class);
+
+Route::get('games/reviews', [GameReviewController::class, 'index']);
+
+Route::prefix('/users')->group( function() {
+	Route::post('/login', [LoginController::class, 'login']);
+});
+
